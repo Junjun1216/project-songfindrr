1dda9d68fccab6457a9987e78ae39968260d5188
unknown <thedanielchenn@gmail.com>
2019-03-28 00:21:04 -0400

    Connected to new backend api. Display are now cards

--- frontend/song-findrr/src/utilities/apiUrl.js
+++ frontend/song-findrr/src/utilities/apiUrl.js
@@ -1,3 +1,6 @@
 export const local_url = 'http://localhost:3001/'
 
 export const search_lyrics = 'api/lyrics';
+export const cross_search = 'api/crossSearch/'
+export const fetch_lyrics = 'api/fetchLyrics/';
+
