47946d5b70ce56a2a68259dd6abe5847551a825a
Daniel Chen <danielchen@Daniels-MacBook-Pro-2.local>
2019-03-28 23:55:24 -0400

    User is able to now fetch and display lyrics in a pop up

--- frontend/song-findrr/src/App.js
+++ frontend/song-findrr/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import MainPage from './components/MainPage'
-import LyricResults from './components/LyricResults'
 import store from './store.js'
 
 class App extends Component {
